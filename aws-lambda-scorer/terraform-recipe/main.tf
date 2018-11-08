4aa2f7330968368ab8c0eedcfa67287e29b916f6
Ondrej Sery <ondrej.sery@h2o.ai>
2018-11-08 15:12:38 +0100

    Add skeleton of an AWS lambda DAI deployment Terraform script.
    Wip on #2721.

---
+++ aws-lambda-scorer/terraform-recipe/main.tf
@@ -0,0 +1,57 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "lambda_id" {
+  default = "dai_lambda"
+}
+variable "lambda_zip_path" {
+  default = "../lambda-template/build/distributions/lambda-template.zip"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_lambda_function" "dai_lambda" {
+  function_name = "${var.lambda_id}_function"
+  filename = "${var.lambda_zip_path}"
+  handler = "ai.h2o.deploy.aws.lambda.MojoScorer::handleRequest"
+  source_code_hash = "${base64sha256(file(var.lambda_zip_path))}"
+  role = "${aws_iam_role.dai_lambda_iam_role.arn}"
+  runtime = "java8"
+
+  // Increase resource constraints from the defaults of 3s and 128MB.
+  timeout = 120
+  memory_size = 1024
+
+  environment {
+    variables = {
+      // TODO(osery): Pass in a location of the mojo in S3.
+    }
+  }
+}
+
+# IAM role which dictates what other AWS services the Lambda function may access.
+resource "aws_iam_role" "dai_lambda_iam_role" {
+  name = "${var.lambda_id}_iam_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
