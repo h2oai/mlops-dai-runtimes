dd1cc1c8d76a034c304017bbb2c688abfac3f94e
Ondrej Sery <ondrej.sery@h2o.ai>
2018-11-12 14:15:57 +0100

    Implement Mojo evaluation in AWS lambda.
    Wip on #2721.

---
+++ aws-lambda-scorer/lambda-template/src/main/java/ai/h2o/dia/deploy/aws/lambda/MojoFrameToResponseConverter.java
@@ -0,0 +1,64 @@
+package ai.h2o.dia.deploy.aws.lambda;
+
+import ai.h2o.dai.deploy.aws.lambda.model.Row;
+import ai.h2o.dai.deploy.aws.lambda.model.ScoreRequest;
+import ai.h2o.dai.deploy.aws.lambda.model.ScoreResponse;
+import ai.h2o.mojos.runtime.frame.MojoFrame;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+
+/**
+ * Converts the resulting predicted {@link MojoFrame} into the API response object {@link ScoreResponse}.
+ */
+class MojoFrameToResponseConverter implements BiFunction<MojoFrame, ScoreRequest, ScoreResponse> {
+
+    @Override
+    public ScoreResponse apply(MojoFrame mojoFrame, ScoreRequest scoreRequest) {
+        List<Row> outputRows = Stream.generate(Row::new).limit(mojoFrame.getNrows()).collect(Collectors.toList());
+        copyFilteredInputFields(scoreRequest, outputRows);
+        copyResultFields(mojoFrame, outputRows);
+
+        ScoreResponse response = new ScoreResponse();
+        response.setScore(outputRows);
+        return response;
+    }
+
+    private static void copyFilteredInputFields(ScoreRequest scoreRequest, List<Row> outputRows) {
+        Set<String> includedFields = new HashSet<>(
+                Optional.ofNullable(scoreRequest.getIncludeFieldsInOutput()).orElse(emptyList()));
+        if (includedFields.isEmpty()) {
+            return;
+        }
+        for (int row = 0; row < outputRows.size(); row++) {
+            Row inputRow = scoreRequest.getRows().get(row);
+            Row outputRow = outputRows.get(row);
+            List<String> inputFields = scoreRequest.getFields();
+            for (int col = 0; col < inputFields.size(); col++) {
+                if (includedFields.contains(inputFields.get(col))) {
+                    outputRow.add(inputRow.get(col));
+                }
+            }
+        }
+    }
+
+    private static void copyResultFields(MojoFrame mojoFrame, List<Row> outputRows) {
+        String[][] outputColumns = new String[mojoFrame.getNcols()][];
+        for (int col = 0; col < mojoFrame.getNcols(); col++) {
+            outputColumns[col] = mojoFrame.getColumn(col).getDataAsStrings();
+        }
+        for (int row = 0; row < mojoFrame.getNrows(); row++) {
+            Row outputRow = outputRows.get(row);
+            for (String[] resultColumn : outputColumns) {
+                outputRow.add(resultColumn[row]);
+            }
+        }
+    }
+}
---
+++ aws-lambda-scorer/lambda-template/src/main/java/ai/h2o/dia/deploy/aws/lambda/RequestToMojoFrameConverter.java
@@ -0,0 +1,34 @@
+package ai.h2o.dia.deploy.aws.lambda;
+
+import ai.h2o.dai.deploy.aws.lambda.model.Row;
+import ai.h2o.dai.deploy.aws.lambda.model.ScoreRequest;
+import ai.h2o.mojos.runtime.frame.MojoFrame;
+import ai.h2o.mojos.runtime.frame.MojoFrameBuilder;
+import ai.h2o.mojos.runtime.frame.MojoFrameMeta;
+import ai.h2o.mojos.runtime.frame.MojoRowBuilder;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * Converts the original API request object {@link ScoreRequest} into the input {@link MojoFrame}.
+ */
+class RequestToMojoFrameConverter implements BiFunction<ScoreRequest, MojoFrameMeta, MojoFrame> {
+    @Override
+    public MojoFrame apply(ScoreRequest scoreRequest, MojoFrameMeta meta) {
+        MojoFrameBuilder frameBuilder = new MojoFrameBuilder(meta);
+        List<String> fields = scoreRequest.getFields();
+
+        if (scoreRequest.getRows() != null) {
+            for (Row row : scoreRequest.getRows()) {
+                MojoRowBuilder rowBuilder = frameBuilder.getMojoRowBuilder();
+                for (int i = 0; i < row.size(); i++) {
+                    rowBuilder.setValue(fields.get(i), row.get(i));
+                }
+                frameBuilder.addRow(rowBuilder);
+            }
+        }
+
+        return frameBuilder.toMojoFrame();
+    }
+}
