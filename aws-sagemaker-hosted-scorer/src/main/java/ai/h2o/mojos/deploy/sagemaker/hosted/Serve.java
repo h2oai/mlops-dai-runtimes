834d081cef8143298bc84b188b68899b38eaf9dd
Tom Kraljevic <tomk@tomk.net>
2019-03-29 11:26:00 -0700

    * Improve exception handling behavior based on customer feedback. * Add some comments. * Add isReady().

--- aws-sagemaker-hosted-scorer/src/main/java/ai/h2o/mojos/deploy/sagemaker/hosted/Serve.java
+++ aws-sagemaker-hosted-scorer/src/main/java/ai/h2o/mojos/deploy/sagemaker/hosted/Serve.java
@@ -9,18 +9,23 @@ import ai.h2o.mojos.runtime.frame.MojoFrame;
 import ai.h2o.mojos.runtime.frame.MojoFrameBuilder;
 import ai.h2o.mojos.runtime.frame.MojoRowBuilder;
 
+import java.io.File;
 import java.util.HashMap;
 
 @Component
 public class Serve {
     // Used to prevent REST API requests before the server is initialized and ready to handle them.
-    volatile boolean ready = false;
-
-    private volatile MojoPipeline model;
+    private volatile boolean ready = false;
 
     private void setReady() {
         ready = true;
     }
+
+    boolean isReady() {
+        return ready;
+    }
+
+    private volatile MojoPipeline model;
 
     @Autowired
     public Serve() {
@@ -31,10 +36,34 @@ public class Serve {
      * Since this object is used as an Autowired Spring object, the constructor call is not actually visible.
      */
     void init() throws Exception {
-        model = MojoPipeline.loadFrom("pipeline.mojo");
+        String[] locationsToCheck = {
+                "/opt/ml/model/pipeline.mojo",
+                "./pipeline.mojo"
+        };
+
+        File f = null;
+        for (String location : locationsToCheck) {
+            File tmp = new File(location);
+            if (tmp.exists()) {
+                f = tmp;
+                break;
+            }
+        }
+
+        if (f == null) {
+            System.out.println("ERROR: pipeline.mojo file not found");
+            System.exit(1);
+        }
+
+        long startMillis = System.currentTimeMillis();
+        model = MojoPipeline.loadFrom(f.getAbsolutePath());
+        long endMillis = System.currentTimeMillis();
+        long deltaMillis = endMillis - startMillis;
+        System.out.println("INFO: MojoPipeline.loadFrom took " + deltaMillis + " millis");
     }
 
     HashMap<String,String> invocations(HashMap<String,String> inputs) {
+        // Put the input variables into the iframe.
         MojoFrameBuilder frameBuilder = model.getInputFrameBuilder();
         MojoRowBuilder rowBuilder = frameBuilder.getMojoRowBuilder();
         for (String key : inputs.keySet()) {
@@ -44,8 +73,10 @@ public class Serve {
         frameBuilder.addRow(rowBuilder);
         MojoFrame iframe = frameBuilder.toMojoFrame();
 
+        // Make the prediction.  Prediction outputs go to the oframe.
         MojoFrame oframe = model.transform(iframe);
 
+        // Move the oframe fields into a more standard Java object for the response.
         @SuppressWarnings("unchecked")
         HashMap<String,String> response = new HashMap();
         SimpleCSV outCsv = SimpleCSV.read(oframe);
