65af2b0333c45db8fcba5c1410b4ca98e01f8faa
Tom Kraljevic <tomk@tomk.net>
2019-03-29 11:26:00 -0700

    * Improve exception handling behavior based on customer feedback. * Add some comments. * Add isReady().

--- aws-sagemaker-hosted-scorer/src/main/java/ai/h2o/mojos/deploy/sagemaker/hosted/ServeController.java
+++ aws-sagemaker-hosted-scorer/src/main/java/ai/h2o/mojos/deploy/sagemaker/hosted/ServeController.java
@@ -20,9 +20,29 @@ public class ServeController {
     @Autowired
     private Serve sv = null;
 
+    /**
+     * Performs an inference.
+     *
+     * The request payload body is a single json object representing one row with string names and string values.
+     * Numerical values should still be represented as a string.  e.g.  "1" or "1.234".
+     *
+     * The response payload body is a single json object with string names and string values.
+     *
+     * The HTTP response is one of:
+     *     SC_OK (200)                    -- A successful prediction was made
+     *     SC_BAD_REQUEST (400)           -- Input error
+     *     SC_INTERNAL_SERVER_ERROR (500) -- Server-side error
+     *
+     * See the SageMaker documentation here:
+     *     See https://docs.aws.amazon.com/sagemaker/latest/dg/your-algorithms-inference-code.html
+     *
+     * @param request One input row in json format
+     * @param response One output row in json format
+     * @return If 200 response, one prediction in json format.
+     */
     @SuppressWarnings("unused")
     @RequestMapping("/invocations")
-    public HashMap<String,String> invocations(HttpServletRequest request) {
+    public HashMap<String,String> invocations(HttpServletRequest request, HttpServletResponse response) {
         @SuppressWarnings("unchecked")
         HashMap<String,String> inputs = new HashMap();
 
@@ -37,20 +57,41 @@ public class ServeController {
                     String value = entry.getValue().getAsString();
                     inputs.put(key, value);
                 }
-                catch (Exception ignore) {
+                catch (Exception e) {
+                    e.printStackTrace();
+                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    return null;
                 }
             }
         }
-        catch (Exception ignore) {
+        catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return null;
         }
 
-        return sv.invocations(inputs);
+        try {
+            return sv.invocations(inputs);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return null;
+        }
     }
 
+    /**
+     * Perform a health/readiness check.
+     *
+     * See the SageMaker documentation here:
+     *     See https://docs.aws.amazon.com/sagemaker/latest/dg/your-algorithms-inference-code.html
+     *
+     * @param response 200 if the service is available; 504 otherwise.
+     */
     @SuppressWarnings("unused")
     @RequestMapping("/ping")
-    public void ping(HttpServletRequest request, HttpServletResponse response) {
-        if (sv.ready) {
+    public void ping(HttpServletResponse response) {
+        if (sv.isReady()) {
             response.setStatus(HttpServletResponse.SC_OK);
         }
         else {
