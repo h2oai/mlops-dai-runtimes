c7d93621392fcddcb07122bb8d96b2c9bbca7bbb
Jorge Hernandez Villapol <jherna.villapol@gmail.com>
2019-10-29 15:13:08 -0500

    Update Template

--- azure-ml-deployment/DeployModel.sh
+++ azure-ml-deployment/DeployModel.sh
@@ -8,6 +8,34 @@
 
 az login
 
+# Create Resource Group and Storage Account
+
+az group create --name $resource_group_name --location $location
+
+storage_name=h2ostore${RANDOM}
+
+az storage account create --name $storage_name --resource-group $resource_group_name --location $location --sku Standard_LRS --encryption blob
+
+# Upload packages and modify dai_deploy.yml
+
+storage_key=$(az storage account keys list --account-name $storage_name --resource-group $resource_group_name | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['value'])")
+
+export AZURE_STORAGE_ACCOUNT="${storage_name}"
+export AZURE_STORAGE_KEY="${storage_key}"
+
+az storage container create --name "mojodependencies" --public-access blob
+
+dai_mojo_blob=$(basename -- "$daimojo_path")
+datatable_blob=$(basename -- "$datatable_path")
+az storage blob upload --container-name "mojodependencies" --name $dai_mojo_blob --file $daimojo_path
+dai_mojo_url=$(az storage blob url --container-name "mojodependencies" --name $dai_mojo_blob | tr -d '"')
+az storage blob upload --container-name "mojodependencies" --name $datatable_blob --file $datatable_path
+datatable_url=$(az storage blob url --container-name "mojodependencies" --name $datatable_blob | tr -d '"')
+
+cp dai_deploy_template.yml dai_deploy.yml
+sed -i -E "s,@@DAI MOJO URL@@,${dai_mojo_url}," dai_deploy.yml
+sed -i -E "s,@@DATA TABLE URLL@@,${datatable_url}," dai_deploy.yml
+
 # Create ML workspace 
 
 az ml workspace create -w $workspace_name -g $resource_group_name -l $location  -y --exist-ok
@@ -23,10 +51,7 @@ if [ $deployment_type == "ACI" ]; then
 az ml model deploy -w $workspace_name --resource-group $resource_group_name --name $service_name -m $model_name:1  --ic inferenceconfig.json --dc deploymentconfigACI.json
 elif [ $deployment_type == "AKS" ]; then
 az ml computetarget create aks -n $aks_cluster -w $workspace_name --resource-group $resource_group_name
-az ml model deploy -ct $aks_cluster -w $workspace_name --resource-group $resource_group_name -m $model_name:1 -n $service_name -ic inferenceconfig.json -dc deploymentconfigAKS.json
+az ml model deploy --ct $aks_cluster -w $workspace_name --resource-group $resource_group_name -m $model_name:1 -n $service_name --ic inferenceconfig.json --dc deploymentconfigAKS.json
 else
 az ml model deploy -w $workspace_name --resource-group $resource_group_name -n $service_name -m $model_name:1 --ic inferenceconfig.json --dc deploymentconfigLocal.json
 fi
-
-
-#az ml service get-keys -n $service_name -w "$workspace_name" --resource-group $resource_group_name
--- azure-ml-deployment/Readme.md
+++ azure-ml-deployment/Readme.md
@@ -8,12 +8,45 @@ This template is designed to show how to deploy Driverless AI Mojo models to Azu
 
 - Driverless AI License 
 - Driverless AI Mojo Model
-- An Azure Machine Learning workspace. For more information, see [Create an Azure Machine Learning workspace.] (https://docs.microsoft.com/en-us/azure/machine-learning/service/how-to-manage-workspace)
-- The [Azure Machine Learning SDK for Python](https://docs.microsoft.com/en-us/python/api/overview/azure/ml/intro?view=azure-ml-py)
+- Driverless AI C++ Runtime Mojo and Datatable package - http://docs.h2o.ai/driverless-ai/latest-stable/docs/userguide/scoring-pipeline-cpp.html
+- The [Azure Command Line Intercace]  (https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
 
 
-The following notebook will show you
+#### Add the Machine Learning Extension 
 
-- How to register models to AzureML
-- How to create reusable enviroments to AzureML
-- How to deploy Driverless AI Mojos to AzureML services 
+```sh 
+az extension add -n azure-cli-ml
+```
+
+### Configure Deployment
+
+To configure the deployment modify the following files: 
+
+- config.txt
+    - worspace_name: Name of the Azure Machine Learning Workspace where to deploy the model
+    - resource_group_name: Name of the resource group
+    - location: Azure Region where to deploy all the resources. To get a list of valid locations for your account use: `az account list-locations -o table` 
+    - model_name: Name that will be used to register the model on Azure
+    - description: Short summary about the model
+    - model_path: path to the pipeline.mojo file
+    - deployment_type: Type of the Deployment (ACI, AKS, Local)
+    - aks_cluster: Name of the AKS cluster to be created (only if AKS is selected)
+    - dai_mojo_path: path to the C++ Runtime Mojo
+    - datatable_path: path to the datatable package
+- deploymentconfigACI.json 
+- deploymentconfigAKS.json
+- deploymentconfigLocal.json
+
+### DAI License 
+
+Must be located in the scoring-pipeline/env directory
+
+### Test Deployment 
+
+In order to test the deployment, the service keys are needed. This deployment script uses secret keys as the default authentication. To get the primary and secondary keys use: 
+
+```
+az ml service get-keys -n $service_name -w $workspace_name --resource-group $resource_group_name
+```
+
+The test.py script shows how the web service can be consumed. 
