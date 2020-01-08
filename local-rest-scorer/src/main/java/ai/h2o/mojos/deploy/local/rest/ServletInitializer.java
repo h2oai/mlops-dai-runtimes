8dafced2b00f6c88fe65ab14f5f0a4e1ccbad757
Ondrej Sery <ondrej.sery@h2o.ai>
2020-01-08 10:52:03 +0100

    Enable building war archive.

---
+++ local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/ServletInitializer.java
@@ -0,0 +1,12 @@
+package ai.h2o.mojos.deploy.local.rest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ScorerApplication.class);
+  }
+}
