18251efab9918a9721c4f462c612b2dc838d873c
nkpng2k <nkpng2k@gmail.com>
2020-02-10 16:36:44 -0800

    working implementation of scorer for google cloud run. Minimal code copy. Extends existing local rest scorer

--- gcp-cloud-run/src/main/java/ai/h2o/mojos/deploy/gcp/cloud/run/config/EnvironmentConfiguration.java
+++ gcp-cloud-run/src/main/java/ai/h2o/mojos/deploy/gcp/cloud/run/config/EnvironmentConfiguration.java
@@ -37,20 +37,25 @@ public class EnvironmentConfiguration {
   }
 
   private void downloadFromGcs() {
-    downloadFileFromGcs(System.getenv("MOJO_GCS_PATH"));
-    downloadFileFromGcs(System.getenv("LICENSE_GCS_PATH"));
+    ensureDirectoryExists(mojoDownloadPath);
+    downloadFileFromGcs(getFromEnv("MOJO_GCS_PATH"));
+    ensureDirectoryExists(licenseDownloadPath);
+    downloadFileFromGcs(getFromEnv("LICENSE_GCS_PATH"));
   }
 
   private void downloadFileFromGcs(String filePath) {
-
     Properties fileProps = parseGcsPath(filePath);
+    log.info(
+        String.format(
+            "Parsed GCS Path to - Bucket: %s, Path: %s",
+            fileProps.getProperty("bucket"), fileProps.getProperty("filepath")));
     Blob blob =
         storage.get(BlobId.of(fileProps.getProperty("bucket"), fileProps.getProperty("filepath")));
     if (filePath.endsWith(".mojo")) {
-      log.info("Downloading mojo pipeline");
+      log.info(String.format("Downloading Mojo to: %s/pipeline.mojo", mojoDownloadPath));
       blob.downloadTo(Paths.get(mojoDownloadPath, "pipeline.mojo"));
     } else if (filePath.endsWith(".sig")) {
-      log.info("Downloading license file");
+      log.info(String.format("Downloading license to: %s/license.sig", licenseDownloadPath));
       blob.downloadTo(Paths.get(licenseDownloadPath, "license.sig"));
     } else {
       String errMsg = "Path provided: %s, is not used for this scorer. Not downloading";
@@ -67,7 +72,20 @@ public class EnvironmentConfiguration {
     String gcsPathNoPrefix = gcsPath.replace("gs://", "");
     List<String> gcsPathArray = Splitter.on('/').splitToList(gcsPathNoPrefix);
     properties.setProperty("bucket", gcsPathArray.get(0));
-    properties.setProperty("filepath", gcsPathNoPrefix.replace(gcsPathArray.get(0), ""));
+    properties.setProperty("filepath", gcsPathNoPrefix.replace(gcsPathArray.get(0) + "/", ""));
     return properties;
+  }
+
+  private String getFromEnv(String envVar) {
+    if (System.getenv(envVar) == null) {
+      throw new RuntimeException(
+          String.format("Error: required environment variable: %s, is not set", envVar));
+    }
+    return System.getenv(envVar);
+  }
+
+  private void ensureDirectoryExists(String dirPath) {
+    File dir = new File(dirPath);
+    dir.mkdir();
   }
 }
