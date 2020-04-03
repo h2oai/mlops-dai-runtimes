b0a7ba130170fe344fa4911aaed9031413528b65
Qiang Kou <qkou@qkou.info>
2020-04-03 14:16:34 -0700

    update CsvToMojoFrameConverter using OpenCSV

--- common/transform/src/main/java/ai/h2o/mojos/deploy/common/transform/CsvToMojoFrameConverter.java
+++ common/transform/src/main/java/ai/h2o/mojos/deploy/common/transform/CsvToMojoFrameConverter.java
@@ -1,20 +1,31 @@
 package ai.h2o.mojos.deploy.common.transform;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import ai.h2o.mojos.runtime.frame.MojoFrame;
 import ai.h2o.mojos.runtime.frame.MojoFrameBuilder;
 import ai.h2o.mojos.runtime.frame.MojoRowBuilder;
-import ai.h2o.mojos.runtime.utils.SimpleCSV;
-import java.io.IOException;
+import ai.h2o.mojos.runtime.utils.BatchedCsvMojoProcessor;
+import com.opencsv.CSVReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Iterator;
 
 /** Converts the CSV from {@link InputStream} to the {@link MojoFrame} for scoring. */
 public class CsvToMojoFrameConverter {
   /** Converts the CSV data from the given {@link InputStream} to a {@link MojoFrame}. */
-  public MojoFrame apply(InputStream inputStream, MojoFrameBuilder frameBuilder)
-      throws IOException {
+  public MojoFrame apply(InputStream inputStream, MojoFrameBuilder frameBuilder) {
     MojoRowBuilder rowBuilder = frameBuilder.getMojoRowBuilder();
-    SimpleCSV csv = SimpleCSV.read(inputStream);
-    String[] labels = csv.getLabels();
+
+    Reader reader = new InputStreamReader(inputStream, UTF_8);
+
+    final CSVReader csvReader = BatchedCsvMojoProcessor.readerToCsvReader(reader);
+
+    final Iterator<String[]> csvReaderIter = csvReader.iterator();
+
+    final String[] labels = csvReaderIter.next();
+
     if (labels.length != rowBuilder.size()) {
       throw new IllegalArgumentException(
           String.format(
@@ -22,14 +33,15 @@ public class CsvToMojoFrameConverter {
               labels.length, rowBuilder.size()));
     }
 
-    String[][] data = csv.getData();
-    for (String[] row : data) {
+    for (; csvReaderIter.hasNext(); ) {
+      final String[] row = csvReaderIter.next();
       for (int c = 0; c < row.length; c++) {
         rowBuilder.setValue(labels[c], row[c]);
       }
       // Reuse the builder.
       rowBuilder = frameBuilder.addRow(rowBuilder);
     }
+
     return frameBuilder.toMojoFrame();
   }
 }
