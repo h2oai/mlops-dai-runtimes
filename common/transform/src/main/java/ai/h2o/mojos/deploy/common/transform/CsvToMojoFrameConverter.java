942b9480fccc0463a34f38fd91f782dc9dabf9bf
Qiang Kou (KK) <qkou@qkou.info>
2020-04-05 22:42:21 -0700

    [MOJO] upgrade mojo runtime to support xgboost 1.0.0 (#166)
    * update CsvToMojoFrameConverter using OpenCSV
    
    * Keeping original contract since consumers can rely on it.
    
    * mark CsvToMojoFrameConverter deprecated
    
    Co-authored-by: mmalohlava <michal@h2o.ai>

--- common/transform/src/main/java/ai/h2o/mojos/deploy/common/transform/CsvToMojoFrameConverter.java
+++ common/transform/src/main/java/ai/h2o/mojos/deploy/common/transform/CsvToMojoFrameConverter.java
@@ -1,20 +1,50 @@
 package ai.h2o.mojos.deploy.common.transform;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import ai.h2o.mojos.runtime.frame.MojoFrame;
 import ai.h2o.mojos.runtime.frame.MojoFrameBuilder;
 import ai.h2o.mojos.runtime.frame.MojoRowBuilder;
-import ai.h2o.mojos.runtime.utils.SimpleCSV;
+import ai.h2o.mojos.runtime.utils.BatchedCsvMojoProcessor;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Iterator;
 
-/** Converts the CSV from {@link InputStream} to the {@link MojoFrame} for scoring. */
+/**
+ * Converts the CSV from {@link InputStream} to the {@link MojoFrame} for scoring.
+ *
+ * @deprecated use {@link BatchedCsvMojoProcessor} instead, it is better for large datasets as it
+ *     can process it by batches of configurable size.
+ */
+@Deprecated
 public class CsvToMojoFrameConverter {
-  /** Converts the CSV data from the given {@link InputStream} to a {@link MojoFrame}. */
+  /**
+   * Converts the CSV data from the given {@link InputStream} to a {@link MojoFrame}.
+   *
+   * @deprecated use {@link BatchedCsvMojoProcessor} instead, it is better for large datasets as it
+   *     can process it by batches of configurable size. An example usage is here:
+   *     https://github.com/h2oai/mojo2/blob/master/java/mojo2-runtime-impl/src-examples/main/java/ai/h2o/mojos/ExecuteMojo.java#L67-L77
+   *     .
+   */
+  @Deprecated
   public MojoFrame apply(InputStream inputStream, MojoFrameBuilder frameBuilder)
       throws IOException {
     MojoRowBuilder rowBuilder = frameBuilder.getMojoRowBuilder();
-    SimpleCSV csv = SimpleCSV.read(inputStream);
-    String[] labels = csv.getLabels();
+
+    Reader reader = new InputStreamReader(inputStream, UTF_8);
+    // Use default CSV parser settings.
+    final Iterator<String[]> csvReaderIter =
+        BatchedCsvMojoProcessor.readerToCsvReader(reader).iterator();
+    // Read first row as column labels
+    final String[] labels = csvReaderIter.next();
+
+    // It looks like empty file, so follow original contract and throw IOException
+    if (labels == null) {
+      throw new IOException("Empty csv file!");
+    }
+
     if (labels.length != rowBuilder.size()) {
       throw new IllegalArgumentException(
           String.format(
@@ -22,14 +52,15 @@ public class CsvToMojoFrameConverter {
               labels.length, rowBuilder.size()));
     }
 
-    String[][] data = csv.getData();
-    for (String[] row : data) {
+    for (; csvReaderIter.hasNext(); ) {
+      final String[] row = csvReaderIter.next();
       for (int c = 0; c < row.length; c++) {
         rowBuilder.setValue(labels[c], row[c]);
       }
       // Reuse the builder.
       rowBuilder = frameBuilder.addRow(rowBuilder);
     }
+
     return frameBuilder.toMojoFrame();
   }
 }
