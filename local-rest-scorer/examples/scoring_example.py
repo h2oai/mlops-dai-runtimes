5105f700dafa641a8290b8ca57c6af918f6ff7f1
nkpng2k <nkpng2k@gmail.com>
2020-07-07 16:49:30 -0700

    simple python scoring example for local rest scorer

---
+++ local-rest-scorer/examples/scoring_example.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import requests
+
+
+# Assumes that the python script is running in same location as rest scorer
+# Change <localhost> to proper ip address to get scores from remote scorer
+URL = "http://localhost:8080/model/score"
+HEADERS = "Content-Type: application/json"
+
+sample_df = pd.read_csv("/path/to/data.csv")
+
+
+# Score single row at a time
+for i, row in sample_df.iterrows():
+    # Replace any null values with stringified version
+    # Else will result in json parsing errors
+    row = row.fillna("NaN")
+
+    payload = {"fields": list(sample_df.columns), "rows": [list(row)]}
+    res = requests.post(URL, data=payload, headers=HEADERS)
+
+    print(res.content)
+
+
+# Score all rows in single request
+payload = {"fields": list(sample_df.columns), "rows": sample_df.values.tolist()}
+res = requests.post(URL, data=payload, headers=HEADERS)
+
+print(res.content)
+
