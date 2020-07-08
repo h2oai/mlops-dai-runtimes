a15f461cc2e56d7afb8352f0242109f49dee90b7
nkpng2k <nkpng2k@gmail.com>
2020-07-07 18:58:33 -0700

    small changes for proper functionality.

--- local-rest-scorer/examples/scoring_example.py
+++ local-rest-scorer/examples/scoring_example.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import json
 
 
 # Assumes that the python script is running in same location as rest scorer
@@ -17,7 +18,8 @@ for i, row in sample_df.iterrows():
     row = row.fillna("NaN")
 
     payload = {"fields": list(sample_df.columns), "rows": [list(row)]}
-    res = requests.post(URL, data=payload, headers=HEADERS)
+    json_payload = json.dumps(payload)
+    res = requests.post(URL, data=json_payload, headers=HEADERS)
 
     print(res.content)
 
@@ -27,7 +29,18 @@ for i, row in sample_df.iterrows():
 # Else will result in json parsing errors
 sample_df = sample_df.fillna("NaN")
 payload = {"fields": list(sample_df.columns), "rows": sample_df.values.tolist()}
-res = requests.post(URL, data=payload, headers=HEADERS)
+json_payload = json.dumps(payload)
+res = requests.post(URL, data=json_payload, headers=HEADERS)
+
+print(res.content)
+
+
+# IF YOU HAVE REQUESTS PACKAGE V2.4+
+
+sample_df = pd.read_csv("/path/to/data.csv")
+sample_df = sample_df.fillna("NaN")
+payload = {"fields": list(sample_df.columns), "rows": sample_df.values.tolist()}
+res = requests.post(URL, json=payload)
 
 print(res.content)
 
