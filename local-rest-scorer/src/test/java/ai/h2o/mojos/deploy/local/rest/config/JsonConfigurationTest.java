9232ed868280bc5a663fba6d55828db4f7eb1368
mmalohlava <michal@h2o.ai>
2020-07-08 20:52:31 -0700

    Magic import order 🤦‍♂️

--- local-rest-scorer/src/test/java/ai/h2o/mojos/deploy/local/rest/config/JsonConfigurationTest.java
+++ local-rest-scorer/src/test/java/ai/h2o/mojos/deploy/local/rest/config/JsonConfigurationTest.java
@@ -1,9 +1,17 @@
 package ai.h2o.mojos.deploy.local.rest.config;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import ai.h2o.mojos.deploy.common.rest.model.Row;
+
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,15 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.Stream;
-
-import ai.h2o.mojos.deploy.common.rest.model.Row;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @JsonTest
@@ -33,10 +32,11 @@ class JsonConfigurationTest {
 
   private ObjectMapper defaultObjectMapper = new ObjectMapper();
 
-  
+
   @ParameterizedTest
   @MethodSource
-  public void objectMapperShouldHandlesMixedValueRows(String givenRowJson, Row expRow) throws IOException {
+  public void objectMapperShouldHandlesMixedValueRows(String givenRowJson, Row expRow)
+      throws IOException {
     // When
     Row row = objectMapper.readValue(givenRowJson, Row.class);
 
@@ -44,27 +44,31 @@ class JsonConfigurationTest {
     assertThat(row).containsExactlyElementsIn(expRow);
   }
 
-  @ParameterizedTest
-  @MethodSource
-  public void defaultObjectMapperShouldFailOnNonNumericRows(String givenRowJson, Row expRow) throws IOException {
-    // When & Then
-    assertThrows(JsonMappingException.class, () -> defaultObjectMapper.readValue(givenRowJson, Row.class));
-
-  }
-
   @SuppressWarnings("unused")
   private static Stream<Arguments> objectMapperShouldHandlesMixedValueRows() {
     return Stream.concat(
         Stream.of(
-          Arguments.arguments("[1,2,3]", toRow("1", "2", "3")),
-          Arguments.arguments("[1,\"NA\",3]", toRow("1", "NA", "3")),
-          Arguments.arguments("[1,\"null\",3]", toRow("1", "null", "3")),
-          Arguments.arguments("[]", toRow()),
-          Arguments.arguments("[null]", toRow((String) null)),
-          Arguments.arguments("[1,\"NaN\",3]", toRow("1", "NaN", "3")),
-          Arguments.arguments("[0,1,2,\"Infinity\"]", toRow("0", "1", "2", "Infinity"))
+            Arguments.arguments("[1,2,3]", toRow("1", "2", "3")),
+            Arguments.arguments("[1,\"NA\",3]", toRow("1", "NA", "3")),
+            Arguments.arguments("[1,\"null\",3]", toRow("1", "null", "3")),
+            Arguments.arguments("[]", toRow()),
+            Arguments.arguments("[null]", toRow((String) null)),
+            Arguments.arguments("[1,\"NaN\",3]", toRow("1", "NaN", "3")),
+            Arguments.arguments("[0,1,2,\"Infinity\"]", toRow("0", "1", "2", "Infinity"))
         ),
         defaultObjectMapperShouldFailOnNonNumericRows()
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  public void defaultObjectMapperShouldFailOnNonNumericRows(
+      String givenRowJson,
+      @SuppressWarnings("unused") Row expRow) {
+    // When & Then
+    assertThrows(
+        JsonMappingException.class,
+        () -> defaultObjectMapper.readValue(givenRowJson, Row.class)
     );
   }
 
