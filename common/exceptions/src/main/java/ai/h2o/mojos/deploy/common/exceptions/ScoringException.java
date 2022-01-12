7fe49bd82b9baafd5d7bd2d88601b9fd6158d3d5
nkpng2k <nkpng2k@gmail.com>
2022-01-11 19:30:18 -0800

    remove unused code

--- common/exceptions/src/main/java/ai/h2o/mojos/deploy/common/exceptions/ScoringException.java
+++ common/exceptions/src/main/java/ai/h2o/mojos/deploy/common/exceptions/ScoringException.java
@@ -2,19 +2,7 @@ package ai.h2o.mojos.deploy.common.exceptions;
 
 public class ScoringException extends RuntimeException {
 
-  public ScoringException() {
-    super();
-  }
-
   public ScoringException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  public ScoringException(String message) {
-    super(message);
-  }
-
-  public ScoringException(Throwable cause) {
-    super(cause);
   }
 }
--- common/exceptions/src/main/java/ai/h2o/mojos/deploy/common/exceptions/ScoringShapleyException.java
+++ common/exceptions/src/main/java/ai/h2o/mojos/deploy/common/exceptions/ScoringShapleyException.java
@@ -1,19 +1,8 @@
 package ai.h2o.mojos.deploy.common.exceptions;
 
 public class ScoringShapleyException extends RuntimeException {
-  public ScoringShapleyException() {
-    super();
-  }
 
   public ScoringShapleyException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  public ScoringShapleyException(String message) {
-    super(message);
-  }
-
-  public ScoringShapleyException(Throwable cause) {
-    super(cause);
   }
 }
