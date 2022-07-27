0f108a6d44dae33862a642d5f613a04487e1c361
nkpng2k <nkpng2k@gmail.com>
2022-07-26 18:23:55 -0700

    re-implement image scoring api to local-rest-scorer project

---
+++ local-rest-scorer/src/test/java/ai/h2o/mojos/deploy/local/rest/controller/ModelsApiControllerV1ExpTest.java
@@ -0,0 +1,36 @@
+package ai.h2o.mojos.deploy.local.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import ai.h2o.mojos.deploy.common.rest.v1exp.model.ScoreMediaRequest;
+import ai.h2o.mojos.deploy.common.rest.v1exp.model.ScoreResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class ModelsApiControllerV1ExpTest {
+
+  @Test
+  void verifyScoreMedia_ReturnsUnimplemented() {
+    // Given
+    ScoreMediaRequest request = mock(ScoreMediaRequest.class);
+    List<Resource> files = new ArrayList<>();
+    ModelsApiControllerV1Exp controller = new ModelsApiControllerV1Exp();
+
+    // When
+    ResponseEntity<ScoreResponse> response =
+        controller.getScoreMedia(request, files);
+
+    // Then
+    assertEquals(response.getStatusCode(), HttpStatus.NOT_IMPLEMENTED);
+  }
+}
