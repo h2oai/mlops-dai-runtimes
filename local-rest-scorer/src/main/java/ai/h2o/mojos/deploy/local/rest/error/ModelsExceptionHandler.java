aea8cc488acea3c4ed34e763c5a2d46cb1588ec3
jih147 <jih147@g.harvard.edu>
2023-04-07 17:03:03 -0700

    fix

--- local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ModelsExceptionHandler.java
+++ local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ModelsExceptionHandler.java
@@ -1,18 +1,19 @@
 package ai.h2o.mojos.deploy.local.rest.error;
 
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Objects;
+
 @ControllerAdvice("ai.h2o.mojos.deploy.local.rest")
-@RequestMapping(produces = "application/vnd.error+json")
 public class ModelsExceptionHandler extends ResponseEntityExceptionHandler {
   private static final Logger log = LoggerFactory.getLogger(ModelsExceptionHandler.class);
 
@@ -20,23 +21,30 @@ public class ModelsExceptionHandler extends ResponseEntityExceptionHandler {
    * Custom Exception handler for ResponseStatusException type.
    */
   @ExceptionHandler(ResponseStatusException.class)
-  public ResponseEntity<ErrorResponse> handleResponseStatusException(
+  public ResponseEntity<Object> handleResponseStatusException(
       ResponseStatusException exception, WebRequest request) {
     log.error("Runtime exception occurred : {}", exception.getMessage(), exception);
-    ErrorResponse errorResponse = new ErrorResponse(
-        exception.getStatus().value(), exception.getMessage());
-    return ResponseEntity.status(exception.getStatus()).body(errorResponse);
+    return ResponseEntity
+        .status(exception.getStatus())
+        .body(ImmutableMap
+          .builder()
+          .put("detail", Objects.requireNonNull(exception.getMessage()))
+          .build()
+        );
   }
 
   /**
    * Custom Exception handler for all Exception type.
    */
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorResponse> handleAllException(
+  public ResponseEntity<Object> handleAllException(
       Exception exception, WebRequest request) {
     log.error("Unexpected exception occurred : {}", exception.getMessage(), exception);
-    ErrorResponse errorResponse = new ErrorResponse(
-        HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage());
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(ImmutableMap
+          .builder()
+          .put("detail", exception.getMessage())
+          .build());
   }
 }
