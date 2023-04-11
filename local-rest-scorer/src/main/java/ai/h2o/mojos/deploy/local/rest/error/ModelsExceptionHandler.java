0802af13d49274e38b7109461291b8863bb078c6
jih147 <jih147@g.harvard.edu>
2023-04-10 18:37:23 -0700

    remove unused import

--- local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ModelsExceptionHandler.java
+++ local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ModelsExceptionHandler.java
@@ -3,7 +3,6 @@ package ai.h2o.mojos.deploy.local.rest.error;
 import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
