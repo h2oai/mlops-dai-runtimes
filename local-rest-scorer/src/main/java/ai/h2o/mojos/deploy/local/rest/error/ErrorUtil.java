2c3b6ecf259fb9c0cf7563eb4f070d329b117729
jih147 <jih147@g.harvard.edu>
2023-04-13 12:37:46 -0700

    fix format

--- local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ErrorUtil.java
+++ local-rest-scorer/src/main/java/ai/h2o/mojos/deploy/local/rest/error/ErrorUtil.java
@@ -4,18 +4,18 @@ import org.springframework.http.HttpStatus;
 
 public class ErrorUtil {
 
-    /**
-     * Translate exception type into error response code.
-     */
-    public static HttpStatus translateErrorCode(Exception exception) {
-        if (exception instanceof IllegalArgumentException) {
-            return HttpStatus.BAD_REQUEST;
-        } else if (exception instanceof IllegalStateException) {
-            return HttpStatus.SERVICE_UNAVAILABLE;
-        } else if (exception instanceof UnsupportedOperationException) {
-            return HttpStatus.NOT_IMPLEMENTED;
-        } else {
-            return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
+  /**
+   * Translate exception type into error response code.
+   */
+  public static HttpStatus translateErrorCode(Exception exception) {
+    if (exception instanceof IllegalArgumentException) {
+      return HttpStatus.BAD_REQUEST;
+    } else if (exception instanceof IllegalStateException) {
+      return HttpStatus.SERVICE_UNAVAILABLE;
+    } else if (exception instanceof UnsupportedOperationException) {
+      return HttpStatus.NOT_IMPLEMENTED;
+    } else {
+      return HttpStatus.INTERNAL_SERVER_ERROR;
     }
+  }
 }
